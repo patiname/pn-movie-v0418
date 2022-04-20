67cf5b015aadfa0ed31422637b8e278930f8d68d
patiname <patiname@naver.com>
2022-04-20 16:31:14 +0900

    axios & useEffect & async, await

--- src/App.js
+++ src/App.js
@@ -1,4 +1,37 @@
+import { useEffect, useState } from "react";
+import { movieApi } from "./api";
+
 const App = () => {
+  const [nowPlayingData, setNowPlayingData] = useState();
+  const [popData, setPopData] = useState();
+  const [upComingData, setUpComingData] = useState();
+
+  useEffect(() => {
+    const movieData = async () => {
+      const {
+        data: { results: nowPlayingResult },
+      } = await movieApi.nowPlaying();
+      // console.log(await movieApi.nowPlaying());
+      // console.log(results);
+      setNowPlayingData(nowPlayingResult);
+
+      const {
+        data: { results: popularResult },
+      } = await movieApi.popular();
+      setPopData(popularResult);
+
+      const {
+        data: { results: upComingResult },
+      } = await movieApi.upComing();
+      setUpComingData(upComingResult);
+    };
+    movieData();
+  }, []);
+
+  console.log("현재 상영:", nowPlayingData);
+  console.log("인기 영화:", popData);
+  console.log("개봉 예정:", upComingData);
+
   return <div></div>;
 };
 
--- src/api.js
+++ src/api.js
@@ -1,1 +1,15 @@
+import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "127c866ecb9630df124cbd4e2522af1c",
+    language: "ko-kr",
+  },
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  popular: () => api.get("movie/popular"),
+  upComing: () => api.get("movie/upcoming"),
+};
